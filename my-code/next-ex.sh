a7cf406a1b0d11a9d61602148c0e003b98507b46
nir geller <nir@debian.debian_pc>
2018-01-19 21:19:22 +0200

    Finished basic script for creating of exercise directory.

--- my-code/next-ex.sh
+++ my-code/next-ex.sh
@@ -1,9 +1,16 @@
 #! /bin/zsh
-if [ $# -eq 0]
-then
+# A basic script for creating the next exercise directory.
+# Might change in order to adapt to higher level exercises.
+number_re='^[0-9]+'
+if [[ $# -eq 0 ]] ; then
 	echo 'No Variables passed to script!'
 	exit -1
 else
-	mkdir ex-$1
-	echo 'CFLAGS=-Wall -g\n' > ex-$1/Makefile
+	if [[ $1 =~ $number_re ]] ; then
+		echo 'Creating directory ex-'$1
+		mkdir ex-$1
+		echo 'CFLAGS=-Wall -g\n' > ex-$1/Makefile
+	else
+		echo 'First argument must be a number!'
+	fi
 fi
