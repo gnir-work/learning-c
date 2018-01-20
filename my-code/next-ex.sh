829025c7277c08c57b0a448642c291b882d7bf1c
nir geller <nir@debian.debian_pc>
2018-01-20 02:51:11 +0200

    Made script a little better by generating a better make file.

--- my-code/next-ex.sh
+++ my-code/next-ex.sh
@@ -2,6 +2,7 @@
 # A basic script for creating the next exercise directory.
 # Might change in order to adapt to higher level exercises.
 number_re='^[0-9]+'
+makefile="CFLAGS=-Wall -g\n\nall: ex"$1"\n\nclean:\n\trm -rf ex"$1
 if [[ $# -eq 0 ]] ; then
 	echo 'No Variables passed to script!'
 	exit -1
@@ -9,7 +10,7 @@ else
 	if [[ $1 =~ $number_re ]] ; then
 		echo 'Creating directory ex-'$1
 		mkdir ex-$1
-		echo 'CFLAGS=-Wall -g\n' > ex-$1/Makefile
+		echo $makefile > ex-$1/Makefile
 	else
 		echo 'First argument must be a number!'
 	fi
