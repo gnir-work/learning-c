17175ffb77cdbbf07b342d4769f8ee0db5c583fd
nir geller <nir@debian.debian_pc>
2018-01-21 07:33:01 +0200

    Removed redundant upper letter cases via math.

--- my-code/ex-10/ex10.c
+++ my-code/ex-10/ex10.c
@@ -7,31 +7,29 @@ int main(int argc, char* argv[]) {
 	}
 
 	int i = 0;
+	int distance_between_upper_and_lower = 32;
 	char letter = argv[1][0];
 	for (i = 0; argv[1][i] != '\0'; i++, letter = argv[1][i]) {
+		if (letter < 97) {
+			letter += distance_between_upper_and_lower;
+		}
 		switch(letter) {
 			case 'a':
-			case 'A':
 				printf("%d: 'A'\n", i);
 				break;
 			case 'e':
-			case 'E':
 				printf("%d: 'E'\n", i);
 				break;
 			case 'i':
-			case 'I':
 				printf("%d: 'I'\n", i);
 				break;
 			case 'o':
-			case 'O':
 				printf("%d: 'O'\n", i);
 				break;
 			case 'u':
-			case 'U':
 				printf("%d: 'U'\n", i);	
 				break;
 			case 'y':
-			case 'Y':
 				if (i > 2) {
 					printf("%d: 'Y'\n", i);
 				}
