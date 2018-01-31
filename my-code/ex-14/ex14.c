7917ead7f32189833333b2712e7d5c78f9b5c710
nir geller <nir@debian.debian_pc>
2018-01-31 18:18:37 +0200

    Removed redundant function.

--- my-code/ex-14/ex14.c
+++ my-code/ex-14/ex14.c
@@ -21,17 +21,12 @@ void print_letters(char arg[])
 	for(i = 0; arg[i] != '\0'; i++) {
 		char ch = arg[i];
 
-		if(can_print_it(ch)) {
+		if(isalpha((int)ch) || isblank((int)ch)) {
 			printf("'%c' == %d ", ch, ch);
 		}
 	}
 
 	printf("\n");
-}
-
-int can_print_it(char ch)
-{
-	return isalpha((int)ch) || isblank((int)ch);
 }
 
 int main(int argc, char* argv[]) {	
