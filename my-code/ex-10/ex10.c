1cde18fc9ad694afe744f82900f858eba3d24226
nir geller <nir@debian.debian_pc>
2018-01-31 14:23:57 +0200

    Finished ex10

--- my-code/ex-10/ex10.c
+++ my-code/ex-10/ex10.c
@@ -5,37 +5,42 @@ int main(int argc, char* argv[]) {
 		printf("ERROR: you must pass arguments to the script!\n");
 		return 1;
 	}
-
-	int i = 0;
 	int distance_between_upper_and_lower = 32;
-	char letter = argv[1][0];
-	for (i = 0; argv[1][i] != '\0'; i++, letter = argv[1][i]) {
-		if (letter < 97) {
-			letter += distance_between_upper_and_lower;
-		}
-		switch(letter) {
-			case 'a':
-				printf("%d: 'A'\n", i);
-				break;
-			case 'e':
-				printf("%d: 'E'\n", i);
-				break;
-			case 'i':
-				printf("%d: 'I'\n", i);
-				break;
-			case 'o':
-				printf("%d: 'O'\n", i);
-				break;
-			case 'u':
-				printf("%d: 'U'\n", i);	
-				break;
-			case 'y':
-				if (i > 2) {
-					printf("%d: 'Y'\n", i);
-				}
-				break;
-			default:
-				printf("%d: %c is not a vowel\n", i, letter);
+	int j = 0;
+	int i = 0;
+	for(j = 1; j < argc; j++) {
+		printf("Working on word '%s'.\n", argv[j]);
+		char letter = argv[j][0];
+		for (i = 0; argv[j][i] != '\0'; i++, letter = argv[1][i]) {
+			printf("%d", i);
+			if (letter < 97) {
+				letter += distance_between_upper_and_lower;
+			}
+			printf("------%c-----", letter);
+			switch(letter) {
+				case 'a':
+					printf("%d: 'A'\n", i);
+					break;
+				case 'e':
+					printf("%d: 'E'\n", i);
+					break;
+				case 'i':
+					printf("%d: 'I'\n", i);
+					break;
+				case 'o':
+					printf("%d: 'O'\n", i);
+					break;
+				case 'u':
+					printf("%d: 'U'\n", i);	
+					break;
+				case 'y':
+					if (i > 2) {
+						printf("%d: 'Y'\n", i);
+					}
+					break;
+				default:
+					printf("%d: %c is not a vowel\n", i, letter);
+			}
 		}
 	}
 
