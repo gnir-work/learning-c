84b22edb132002a16a6b26167a0cdb2bf89a1416
nir geller <nir@debian.debian_pc>
2018-02-02 23:07:09 +0200

    Made the code save the struct on the stack,however the name is still a poiter

--- my-code/ex-16/ex16.c
+++ my-code/ex-16/ex16.c
@@ -13,14 +13,13 @@ struct Person {
 /**
  * Creates a person struct and returns a pointer to it.
  */
-struct Person *Person_create(char *name, int age, int height, int weight) {
-	struct Person *who = malloc(sizeof(struct Person));
-	assert(who != NULL);
+struct Person Person_create(char *name, int age, int height, int weight) {
+	struct Person who;
 
-	who->name = strdup(name);
-	who->age = age;
-	who->height = height;
-	who->weight = weight;
+	who.name = strdup(name);
+	who.age = age;
+	who.height = height;
+	who.weight = weight;
 
 	return who;
 }
@@ -30,52 +29,49 @@ struct Person *Person_create(char *name, int age, int height, int weight) {
  * pointer who points to.
  * NOTE: we need to free first the the memory which Person->name points to.
  */
-void Person_destroy(struct Person *who) {
-	assert(who != NULL);
-
-	free(who->name);
-	free(who);
+void Person_destroy(struct Person who) {
+	free(who.name);
 }
 
 /**
  * Prints out the properties of the struct Person.
  */
-void Person_print(struct Person *who) {
-	printf("name: %s\n", who->name);
-	printf("\tAge: %d\n", who->age);
-	printf("\tHeight: %d\n", who->height);
-	printf("\tWeight: %d\n", who->weight);
+void Person_print(struct Person who) {
+	printf("name: %s\n", who.name);
+	printf("\tAge: %d\n", who.age);
+	printf("\tHeight: %d\n", who.height);
+	printf("\tWeight: %d\n", who.weight);
 	printf("\n");
 }
 
 int main(int argc, char *argv[]) {
 	//Create two people
-	struct Person *nir = Person_create("nir", 20, 87, 89);
-	struct Person *oshrit = Person_create("oshrit", 19, 75, 71);
+	struct Person nir = Person_create("nir", 20, 87, 89);
+	struct Person oshrit = Person_create("oshrit", 19, 75, 71);
 
 	//Print them out
 	printf("the size of a Person is: %ld\n", sizeof(struct Person));
 	
-	printf("Nir is at %p in my memory.\n", nir);
+	printf("Nir is at %p in my memory.\n", &nir);
 	Person_print(nir);
 
-	printf("Oshrit is at %p in my memory.\n", oshrit);
+	printf("Oshrit is at %p in my memory.\n", &oshrit);
 	Person_print(oshrit);
 
 	//Make them old
-	nir->age += 20;
-	nir->weight += 15;
-	nir->height -= 2;
+	nir.age += 20;
+	nir.weight += 15;
+	nir.height -= 2;
 
-	oshrit->age += 20;
-	oshrit->weight += 1;
-	oshrit->height -= 1;
+	oshrit.age += 20;
+	oshrit.weight += 1;
+	oshrit.height -= 1;
 
 	//Print them out again.
-	printf("Nir is at %p in my memory.\n", nir);
+	printf("Nir is at %p in my memory.\n", &nir);
 	Person_print(nir);
 
-	printf("Oshrit is at %p in my memory.\n", oshrit);
+	printf("Oshrit is at %p in my memory.\n", &oshrit);
 	Person_print(oshrit);	
 
 	//Destroy them!
